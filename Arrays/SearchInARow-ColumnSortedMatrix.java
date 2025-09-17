77def754abfc6125781bec1bbefec766d732ce63
K L MANDARA <161995948+KLMANDARA@users.noreply.github.com>
2025-09-17 08:58:15 +0530

    Update SearchInARow-ColumnSortedMatrix.java

--- Arrays/SearchInARow-ColumnSortedMatrix.java
+++ Arrays/SearchInARow-ColumnSortedMatrix.java
@@ -1,1 +1,22 @@
+class Solution {
+    public static boolean matSearch(int mat[][], int x) {
+        // your code here
+        int row=0;
+        int col=mat[0].length-1;
+        while(row<mat.length && col>=0){
+            if(mat[row][col]==x){
+                return true;
+            }
+            else if(x<mat[row][col]){
+                    col--;
+                    
+                }
+                else{
+                    row++;
+                }
+            }
+             return false;
+    }
+       
+    }
 
